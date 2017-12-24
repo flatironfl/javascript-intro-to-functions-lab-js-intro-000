8bf3482f2a667421e9555f1c54d0f84b6e37518f
First Last <flatironcamp.tc99@mamber.net>
2017-12-24 04:48:57 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,35 @@
+function shout (incoming) {
+ return incoming.toUpperCase();
+}
+
+function whisper (incoming) {
+  return incoming.toLowerCase();
+}
+
+function logShout(incoming) {
+  console.log(incoming.toUpperCase());
+}
+
+
+function logWhisper(incoming) {
+  console.log(incoming.toLowerCase());
+}
+
+function sayHiToGrandma(phrase) {
+  switch phrase 
+}
+
+
+
+describe('sayHiToGrandma(string)', function() {
+  it('returns "I can\'t hear you!" if `string` is lowercase', function() {
+    expect(sayHiToGrandma('hello')).toEqual("I can't hear you!")
+  })
+
+  it('returns "YES INDEED!" if `string` is uppercase', function() {
+    expect(sayHiToGrandma('HELLO')).toEqual("YES INDEED!")
+  })
+
+  it('returns "I love you, too." if `string` is "I love you, Grandma."`', function() {
+    expect(sayHiToGrandma("I love you, Grandma.")).toEqual("I love you, too.")
+  })
